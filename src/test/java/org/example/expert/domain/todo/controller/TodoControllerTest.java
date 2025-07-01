6b9b257d8aec823e1148531d1ca04199d21df28f
taeung515 <xodndd515@gmail.com>
2025-07-01 09:20:43 +0900

    refactor: 스프링시큐리티 적용에 따른 코드 변경

--- src/test/java/org/example/expert/domain/todo/controller/TodoControllerTest.java
+++ src/test/java/org/example/expert/domain/todo/controller/TodoControllerTest.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.LocalDateTime;
@@ -30,6 +31,7 @@ class TodoControllerTest {
     @MockBean
     private TodoService todoService;
 
+    @WithMockUser
     @Test
     void todo_단건_조회에_성공한다() throws Exception {
         // given
